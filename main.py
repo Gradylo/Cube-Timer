98cf559823ca7443c7cbd774fa351e0574df79c2
Adam <adam@localhost.localdomain>
2017-07-13 22:51:04 -0400

    Commented some code, minor changes

--- main.py
+++ main.py
@@ -11,23 +11,25 @@ X_DIM = 1500
 Y_DIM = 1000
 
 
-
+'''
+PageState is essentially an enum which helps keep track of
+the state of the application.
+'''
 class PageState(object):
     TIME = 1
     PAUSED = 2
 
-class myTimer(wx.Timer):
-    def Notify(et, st):
-        self.elapsed_time = time.clock() - self.start_time
-        self.time_text.SetLabel(str(self.elapsed_time))
-
-
+'''
+Our subclass of wx.Frame.
+'''
 class Page(wx.Frame):
 
     def __init__(self, parent, title):
         super(Page, self).__init__(parent, title=title,
             size=(X_DIM, Y_DIM))
 
+
+        #initialize several different fonts
         timer_font = wx.Font(48, wx.ROMAN, wx.NORMAL, wx.NORMAL)
         lareger_font = wx.Font(20, wx.ROMAN, wx.NORMAL, wx.NORMAL)
         normal_font = wx.Font(14, wx.ROMAN, wx.NORMAL, wx.NORMAL)
@@ -39,28 +41,29 @@ class Page(wx.Frame):
         self.panel = wx.Panel(self)
         self.panel.SetBackgroundColour('BLACK')
 
-        text = "hi how are you"
-
+        #pagestate keeps track of timer mode
         self.page_state = PageState.PAUSED
 
+        #times is the set which will keep track of all cube times
         self.times = objects.Set()
+        self.times_list = ""
 
 
         #for now
         self.start_time = time.time()
         self.elapsed_time = 0.0
 
+        #time_text is a StaticText for the timer text
         self.time_text = wx.StaticText(self.panel,-1,pos = (X_DIM / 2, 200), style = wx.ALIGN_CENTER_HORIZONTAL)
         self.time_text.SetFont(timer_font)
         self.time_text.SetLabel(str(self.elapsed_time))
 
+        #summary_text is a StaticText for a brief summary or overview of the users's stats
         self.summary_text = wx.StaticText(self.panel, -1, pos=(X_DIM / 2, 500), style=wx.ALIGN_CENTER_HORIZONTAL)
         self.summary_text.SetFont(lareger_font)
         self.summary_text.SetLabel(self.get_summary_text())
 
-        #self.stats_text = wx.TextCtrl(self.panel, value = "ReadOnly Text",
-   #style = wx.TE_READONLY | wx.TE_CENTER )
-
+        #stats_text is a StaticText for the users's in-depth statistics
         self.stats_text = wx.StaticText(self.panel, -1, pos = (X_DIM - 300, 100))
         self.stats_text.SetFont(normal_font)
         self.stats_text.SetLabel(self.get_stats_text())
@@ -68,8 +71,12 @@ class Page(wx.Frame):
         self.ln = wx.StaticLine(self.panel, -1, pos=(X_DIM - 350, 0), size=(0, Y_DIM), style=wx.LI_VERTICAL)
         #self.ln.SetSize((30,30))
 
+        #timer helps us update continually, by sending EVT_TIMER events
+        #to be handled by update()
         self.timer = wx.Timer(self)
-        print(self.timer.Start(1))
+        self.timer.Start(1)
+
+        #Bind actions to their handlers
         self.Bind(wx.EVT_TIMER, self.update, self.timer)
         self.panel.Bind(wx.EVT_KEY_DOWN, self.onKeyPress)
 
@@ -80,12 +87,12 @@ class Page(wx.Frame):
             + "\nSession Mean: " + "{0:.2f}".format(self.times.get_mean())
 
     def get_stats_text(self):
-        return "Current ao5: " + "{0:.2f}".format(self.times.curr_avg5()) + "\nCurrent ao12: " \
-            + "{0:.2f}".format(self.times.curr_avg12()) + "\nCurrent ao100: " + "{0:.2f}".format(self.times.curr_avg100()) \
+        return "Solves: " + str(self.times.get_count()) +  "\nCurrent ao5: " + "{0:.2f}".format(self.times.curr_avg5()) \
+            + "\nCurrent ao12: " + "{0:.2f}".format(self.times.curr_avg12()) + "\nCurrent ao100: " \
+            + "{0:.2f}".format(self.times.curr_avg100()) \
             + "\n\nBest ao5: " + "{0:.2f}".format(self.times.best_avg5()) + "\nBest ao12: " \
             + "{0:.2f}".format(self.times.best_avg12()) + "\nBest ao100: " \
-            + "{0:.2f}".format(self.times.best_avg100())
-
+            + "{0:.2f}".format(self.times.best_avg100()) + "\n\nTimes: " + self.times_list
 
 
     def update(self, event):
@@ -101,8 +108,15 @@ class Page(wx.Frame):
     def stop(self):
         self.page_state = PageState.PAUSED
         self.times.add_time(self.elapsed_time)
+
+        #add time to times list for display
+        if self.times.get_count() > 1:
+            self.times_list += ", "
+        self.times_list += "{0:.2f}".format(self.elapsed_time)
+
         self.summary_text.SetLabel(self.get_summary_text())
         self.stats_text.SetLabel(self.get_stats_text())
+        self.stats_text.Wrap(300)
 
 
     def onKeyPress(self, event):
