8eed080d58b2094ae86131a2bfd4a8b5d6f17f02
Adam <adam@localhost.localdomain>
2017-07-13 21:06:14 -0400

    added stats column

--- main.py
+++ main.py
@@ -58,6 +58,16 @@ class Page(wx.Frame):
         self.summary_text.SetFont(lareger_font)
         self.summary_text.SetLabel(self.get_summary_text())
 
+        #self.stats_text = wx.TextCtrl(self.panel, value = "ReadOnly Text",
+   #style = wx.TE_READONLY | wx.TE_CENTER )
+
+        self.stats_text = wx.StaticText(self.panel, -1, pos = (X_DIM - 300, 100))
+        self.stats_text.SetFont(normal_font)
+        self.stats_text.SetLabel(self.get_stats_text())
+
+        self.ln = wx.StaticLine(self.panel, -1, pos=(X_DIM - 350, 0), size=(0, Y_DIM), style=wx.LI_VERTICAL)
+        #self.ln.SetSize((30,30))
+
         self.timer = wx.Timer(self)
         print(self.timer.Start(1))
         self.Bind(wx.EVT_TIMER, self.update, self.timer)
@@ -66,7 +76,16 @@ class Page(wx.Frame):
 
 
     def get_summary_text(self):
-        return "Session Average: " + "{0:.2f}".format(self.times.get_average()) + "\nSession Mean: " + "{0:.2f}".format(self.times.get_mean())
+        return "Session Average: " + "{0:.2f}".format(self.times.get_average()) \
+            + "\nSession Mean: " + "{0:.2f}".format(self.times.get_mean())
+
+    def get_stats_text(self):
+        return "Current ao5: " + "{0:.2f}".format(self.times.curr_avg5()) + "\nCurrent ao12: " \
+            + "{0:.2f}".format(self.times.curr_avg12()) + "\nCurrent ao100: " + "{0:.2f}".format(self.times.curr_avg100()) \
+            + "\n\nBest ao5: " + "{0:.2f}".format(self.times.best_avg5()) + "\nBest ao12: " \
+            + "{0:.2f}".format(self.times.best_avg12()) + "\nBest ao100: " \
+            + "{0:.2f}".format(self.times.best_avg100())
+
 
 
     def update(self, event):
@@ -83,6 +102,7 @@ class Page(wx.Frame):
         self.page_state = PageState.PAUSED
         self.times.add_time(self.elapsed_time)
         self.summary_text.SetLabel(self.get_summary_text())
+        self.stats_text.SetLabel(self.get_stats_text())
 
 
     def onKeyPress(self, event):
